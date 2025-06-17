4c5210faf164367cbc8e2b8d359938614093a33d
NCSUAL <ncsual@naver.com>
2025-06-17 20:28:13 +0900

    [Test] Write test code for Branch CRUD

--- src/test/java/branch/BranchServiceTest.java
+++ src/test/java/branch/BranchServiceTest.java
@@ -1,11 +1,19 @@
 package branch;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import lsh.security.constant.nested.CityType;
+import lsh.security.domain.Branch;
+import lsh.security.dto.request.BranchRequest;
 import lsh.security.repository.BranchRepository;
 import lsh.security.service.branch.BranchService;
 
@@ -19,6 +27,68 @@ public class BranchServiceTest {
 
     @Test
     public void CRUD_CREATE_기능_테스트(){
+        //given
+        Long id = 100L;
+        String name = "HyunDai";
+        CityType cityType = CityType.SEOUL;
+
+        BranchRequest branchRequest = new BranchRequest(id, name, cityType);
         
+        //when
+        when(branchRepository.save(any(Branch.class))).thenReturn(branchRequest.toEntity());
+
+        branchService.createBranch(branchRequest);
+
+        //then
+        verify(branchRepository).save(any(Branch.class));
     }
+
+    @Test
+    public void CRUD_UPDATE_기능_테스트(){
+        //given
+
+        //기존 엔티티
+        Branch branch = Branch.builder()
+            .id(100L)
+            .name("NAVER")
+            .cityType(CityType.SEOUL)
+            .build();
+
+        //요청 Path
+        Long id = branch.getId();
+
+        //요청 Json
+        String name = "HyunDai";
+        CityType cityType = CityType.SEOUL;
+
+        BranchRequest branchRequest = new BranchRequest(id, name, cityType);
+
+        //when
+        when(branchRepository.findById(id)).thenReturn(Optional.of(branch));
+
+        when(branchRepository.save(any(Branch.class))).thenReturn(branchRequest.toEntity());
+
+        //then
+        Branch newBranch = branchService.isPresent(id, present -> branchService.updateByBranchRequest(present, branchRequest));
+
+        assertEquals(branchRequest.toEntity(), newBranch);
+    }
+
+
+    @Test
+    public void CRUD_DELETE_기능_테스트(){
+        //given
+        Branch branch = Branch.builder()
+        .name("KakaoCar")
+        .cityType(CityType.SEOUL)
+        .build();
+
+        //when
+        doNothing().when(branchRepository).delete(branch);
+        branchService.delete(branch);
+
+        //then
+        verify(branchRepository).delete(branch);
+    }
+
 }
